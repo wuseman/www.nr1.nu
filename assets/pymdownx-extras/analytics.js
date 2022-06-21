2898a04808ea427d0581a52ceda702e8525d5ffe
wuseman <wuseman@nr1.nu>
2022-06-21 10:37:07 +0000

    Deployed f319efb with MkDocs version: 1.3.0

--- assets/pymdownx-extras/analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-B5KK88V0K4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-B5KK88V0K4');
-</script>
