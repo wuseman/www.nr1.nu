a57ab78e7ba8eb074e27b43af0afc2c0e45c4ae4
wuseman <wuseman@nr1.nu>
2022-06-26 22:47:40 +0000

    Deployed fc4d2db with MkDocs version: 1.3.0

---
+++ assets/pymdownx-extras/m.js
@@ -0,0 +1,17 @@
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.www.gentoo.nr1.nu"]);
+  _paq.push(["setDomains", ["*.www.gentoo.nr1.nu","*.adb-cheatsheet.nr1.nu","*.adb.nr1.nu","*.android.nr1.nu","*.archive.nr1.nu","*.boose.nr1.nu","*.bruteforce.nr1.nu","*.camera.nr1.nu","*.chromebook.nr1.nu","*.chromebook1.nr1.nu","*.cisco.nr1.nu","*.cracking.nr1.nu","*.cv.nr1.nu","*.emagnet.nr1.nu","*.hacking.nr1.nu","*.imap.nr1.nu","*.myip.nr1.nu","*.onelinerz.nr1.nu","*.paste.nr1.nu","*.sagemcom.nr1.nu","*.smtp.nr1.nu","*.speedtest.nr1.nu","*.synology.nr1.nu","*.synology1.nr1.nu","*.syslog.nr1.nu","*.technicolor.nr1.nu","*.u.nr1.nu","*.vim.nr1.nu","*.vnstati.nr1.nu","*.w.nr1.nu","*.wbruter.nr1.nu","*.whois.nr1.nu","*.wiki.nr1.nu","*.wloader.nr1.nu","*.wuseman.nr1.nu","*.www.adb-cheatsheet.nr1.nu","*.www.adb.nr1.nu","*.www.android.nr1.nu","*.www.archive.nr1.nu","*.www.boose.nr1.nu","*.www.bruteforce.nr1.nu","*.www.camera.nr1.nu","*.www.chromebook.nr1.nu","*.www.chromebook1.nr1.nu","*.www.cisco.nr1.nu","*.www.cracking.nr1.nu","*.www.cv.nr1.nu","*.www.emagnet.nr1.nu","*.www.hacking.nr1.nu","*.www.imap.nr1.nu","*.www.myip.nr1.nu","*.www.nr1.nu","*.www.onelinerz.nr1.nu","*.www.paste.nr1.nu","*.www.sagemcom.nr1.nu","*.www.smtp.nr1.nu","*.www.speedtest.nr1.nu","*.www.synology.nr1.nu","*.www.synology1.nr1.nu","*.www.syslog.nr1.nu","*.www.technicolor.nr1.nu","*.www.u.nr1.nu","*.www.vim.nr1.nu","*.www.vnstati.nr1.nu","*.www.w.nr1.nu","*.www.wbruter.nr1.nu","*.www.whois.nr1.nu","*.www.wiki.nr1.nu","*.www.wloader.nr1.nu","*.www.wuseman.nr1.nu","*.www.znc.nr1.nu","*.znc.nr1.nu"]]);
+  _paq.push(["enableCrossDomainLinking"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//www.nr1.nu/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
